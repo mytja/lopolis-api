1577048dc94a2d9cf407f8a966768d68799f0be8
mytja <mytja@protonmail.com>
2022-10-01 20:34:19 +0200

    Improve the API

---
+++ lopolis/api_lib.py
@@ -0,0 +1,277 @@
+import re
+
+import httpx
+from bs4 import BeautifulSoup
+
+HOST = "https://web.lopolis.si"
+
+
+class LoPolisAPI:
+    def __init__(self):
+        self.oseba_model = None
+        self.ustanova_id = None
+        self.oseba_id = None
+        self.oseba_tip = None
+        self.client = httpx.AsyncClient()
+
+    async def get_token(self, username, password):
+        json_data = {
+            "Uporabnik": username,
+            "Geslo": password,
+            "OsveziURL": "",
+        }
+
+        response = await self.client.post("https://web.lopolis.si/Uporab/Prijava2", data=json_data)
+
+        if response.status_code != 301:
+            raise Exception({"error": True, "status_code": response.status_code})
+        if self.client.cookies.get(".LopolisPortalAuth") is None:
+            raise Exception("Unauthorized")
+
+        await self.client.get(HOST)
+        response = await self.client.get(f"{HOST}/?MeniZgorajID=6&MeniID=78")
+        soup = BeautifulSoup(response.text, "html.parser")
+        self.oseba_model = soup.find(id="OsebaModel_ddlOseba").find("option").attrs["value"]
+        try:
+            self.oseba_id, self.oseba_tip, self.ustanova_id = self.oseba_model.split(";")
+        except:
+            raise Exception("Unauthorized")
+
+    async def get_menus(self, year, month):
+        response = await self.client.get(f"{HOST}/Prehrana/Prednarocanje")
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        response = await self.client.post(f"{HOST}/Prehrana/Prednarocanje", data=json_data)
+        soup = BeautifulSoup(response.text, "html.parser")
+        meals = soup.find("tbody").find_all("tr")
+
+        menu = {}
+
+        if meals is None:
+            raise Exception("Unauthorized")
+        for tr in meals:
+            day_output = {}
+            tds = tr.find_all("td")
+
+            day_output["meal"] = tds[1].text.strip()
+            day_output["menu_type"] = tds[2].text.strip()
+            day_output["location"] = tds[3].text.strip()
+            day_output["menu_options"] = []
+
+            menu_content = tds[4]
+
+            day_output["local_id"] = int(menu_content.find(id=re.compile(r"PrednarocanjeItems_(.*)__ABO_PrijavaID")).get("id").split("_")[1])
+            day_output["id"] = menu_content.find(id=re.compile(r"PrednarocanjeItems_(.*)__ABO_PrijavaID")).attrs["value"]
+
+            menu_options = [x for x in menu_content.contents[0].contents if x != "\n"]
+
+            date = menu_content.contents[2]["value"]
+            empty = False
+
+            if menu.get(date) is None:
+                menu[date] = []
+
+            if menu_content.contents[0].get("readonly") != "readonly":
+                day_output["readonly"] = False
+                if len(menu_options) > 1:
+                    for option in menu_options:
+                        if option.get("value") != "":
+                            menu_output = {"value": option.get("value"), "text": option.contents[0],
+                                           "selected": (
+                                               not option.get("selected") is None
+                                           )}
+                            day_output["menu_options"].append(menu_output)
+                else:
+                    empty = True
+            else:
+                menu_output = None
+                for option in menu_options:
+                    if (
+                            not option.get("value") == ""
+                            and not option.get("selected") is None
+                            and not option.contents[0] == ""
+                    ):
+                        menu_output = {
+                            "value": option.get("value"),
+                            "text": option.contents[0],
+                            "selected": True,
+                        }
+                        day_output["readonly"] = True
+
+                if menu_output is not None:
+                    day_output["menu_options"].append(menu_output)
+                else:
+                    empty = True
+
+            if not empty:
+                menu[date].append(day_output)
+
+        return menu
+
+    async def get_checkouts(self, year, month):
+        response = await self.client.get(f"{HOST}/Prehrana/Odjava")
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        response = await self.client.post(f"{HOST}/Prehrana/Odjava", data=json_data)
+        soup = BeautifulSoup(response.text, "html.parser")
+        meals = soup.find("tbody").find_all("tr")
+        checkouts = {}
+        for tr in meals:
+            tds = tr.find_all("td")
+            checkbox_contents = tds[3].find_all("input")
+            date = tds[0].text.strip()
+            if checkouts.get(date) is None:
+                checkouts[date] = []
+            checkouts[date].append(
+                {
+                    "meal_type": tds[1].contents[0].split("\r")[0],
+                    "disabled": checkbox_contents[0].get("disabled") is not None,
+                    "cancelled": checkbox_contents[0].get("checked") is not None,
+                    "location": tds[2].text.strip(),
+                    "date": tds[0].text.strip(),
+                    "full_date": checkbox_contents[2].attrs["value"].strip(),
+                    "checkout_type": tds[4].text.strip(),
+                    "checkout_id": checkbox_contents[3].attrs["value"],
+                    "checkout_local_id": int(checkbox_contents[0].get("id").split("_")[1]),
+                }
+            )
+
+        return checkouts
+
+    # choices is the checkout object (dict) from before.
+    async def set_checkouts(self, year, month, choices):
+        response = await self.client.get(f"{HOST}/Prehrana/Odjava")
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        response = await self.client.post(f"{HOST}/Prehrana/Odjava", data=json_data)
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "Shrani": "Shrani",
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "Shrani",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        for day in choices.values():
+            for i in day:
+                full_date = i["full_date"]
+                cancelled = i["cancelled"]
+                checkout_id = i["checkout_id"]
+                checkout_local_id = i["checkout_local_id"]
+                disabled = i["disabled"]
+
+                odjava_item = f"OdjavaItems[{checkout_local_id}]."
+                if bool(cancelled):
+                    json_data[f"{odjava_item}CheckOut"] = ["true", "false"]
+                else:
+                    json_data[f"{odjava_item}CheckOut"] = "false"
+                json_data[f"{odjava_item}Datum"] = full_date
+                json_data[f"{odjava_item}ABO_PrijavaID"] = checkout_id
+                json_data[f"{odjava_item}ReadOnly"] = "True" if disabled else "False"
+
+        response = await self.client.post(f"{HOST}/Prehrana/Odjava", data=json_data)
+        return response.status_code
+
+    async def set_menus(self, year, month, choices):
+        response = await self.client.get(f"{HOST}/Prehrana/Prednarocanje")
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        response = await self.client.post(f"{HOST}/Prehrana/Prednarocanje", data=json_data)
+        soup = BeautifulSoup(response.text, "html.parser")
+        verification_token = soup.find("input", {"name": "__RequestVerificationToken"}).attrs["value"]
+
+        json_data = {
+            "Shrani": "Shrani",
+            "__RequestVerificationToken": verification_token,
+            "Ukaz": "Shrani",
+            "OsebaModel.ddlOseba": self.oseba_model,
+            "OsebaModel.OsebaID": self.oseba_id,
+            "OsebaModel.OsebaTipID": self.oseba_tip,
+            "OsebaModel.UstanovaID": self.ustanova_id,
+            "MesecModel.Mesec": month,
+            "MesecModel.Leto": year,
+            "X-Requested-With": "XMLHttpRequest",
+        }
+
+        for i, (k, v) in enumerate(choices.items()):
+            full_date = k
+            for meal in v:
+                id = meal["id"]
+                local_id = meal["local_id"]
+                disabled = meal["readonly"]
+
+                selected = ""
+                for menu in meal["menu_options"]:
+                    if menu["selected"]:
+                        selected = menu["value"]
+                        break
+
+                item = f"PrednarocanjeItems[{local_id}]."
+                if not disabled:
+                    json_data[f"{item}MeniIDSkupinaID"] = selected
+                json_data[f"{item}Datum"] = full_date
+                json_data[f"{item}ABO_PrijavaID"] = id
+                json_data[f"{item}ReadOnly"] = "True" if disabled else "False"
+
+        response = await self.client.post(f"{HOST}/Prehrana/Prednarocanje", data=json_data)
+        return response.status_code
--- main.py
+++ main.py
@@ -1,192 +1,20 @@
-import json
+import asyncio
 import os
 
-from flask import Flask, redirect, request
-from flask_cors import CORS, cross_origin
-from user_agents import parse
+from lopolis import LoPolisAPI
 
-import api_lib
-from functions import About, BadRequest, JSONResponse
-from helpers import GetCoffee, HTMLGetCoffee, ImATeapot
+async def main():
+    lopolis = LoPolisAPI()
+    print("Logging in")
+    await lopolis.get_token(os.environ.get("LOPOLIS_USERNAME"), os.environ.get("LOPOLIS_PASSWORD"))
+    print("Successfully logged in")
+    menus = await lopolis.get_menus("2022", "10")
+    print(menus)
+    menus["2022-10-12T00:00:00.0000000"][0]["menu_options"][0]["selected"] = True
+    print(await lopolis.set_menus("2022", "10", menus))
+    checkouts = await lopolis.get_checkouts("2022", "10")
+    print(checkouts)
+    checkouts[list(checkouts.keys())[0]][0]["cancelled"] = True
+    print(await lopolis.set_checkouts("2022", "10", checkouts))
 
-app = Flask(__name__)
-CORS(app)
-
-
-@app.route("/gettoken", methods=["POST"])
-@cross_origin()
-def gettoken():
-
-    content = request.get_json()
-
-    if content is None:
-        return JSONResponse(BadRequest)
-
-    if len(content) == 2 and "username" in content and "password" in content:
-        login_result = api_lib.GetToken(content["username"], content["password"])
-        return JSONResponse(login_result)
-    else:
-        return JSONResponse(BadRequest)
-
-
-@app.route("/getmenus", methods=["POST"])
-@cross_origin()
-def getmenus():
-
-    content = request.get_json()
-
-    if content is None:
-        return JSONResponse(BadRequest)
-
-    if (
-        len(content) == 2
-        and "year" in content
-        and "month" in content
-        and not request.headers.get("Authorization") is None
-    ):
-        try:
-            authorization_token = request.headers.get("Authorization").split("Bearer ")[
-                1
-            ]
-            if len(authorization_token) != 288:
-                raise ValueError("Token not the right length")
-        except:
-            return JSONResponse(BadRequest)
-
-        menus_result = api_lib.GetMenus(
-            authorization_token, content["year"], content["month"]
-        )
-        return JSONResponse(menus_result)
-    else:
-        return JSONResponse(BadRequest)
-
-
-@app.route("/getcheckouts", methods=["POST"])
-@cross_origin()
-def getcheckouts():
-
-    content = request.get_json()
-
-    if content is None:
-        return JSONResponse(BadRequest)
-
-    if (
-        len(content) == 2
-        and "year" in content
-        and "month" in content
-        and not request.headers.get("Authorization") is None
-    ):
-        try:
-            authorization_token = request.headers.get("Authorization").split("Bearer ")[
-                1
-            ]
-            if len(authorization_token) != 288:
-                raise ValueError("Invalid token length")
-        except:
-            return JSONResponse(BadRequest)
-
-        checkouts_result = api_lib.GetCheckouts(
-            authorization_token, content["year"], content["month"]
-        )
-        return JSONResponse(checkouts_result)
-    else:
-        return JSONResponse(BadRequest)
-
-
-@app.route("/setmenus", methods=["POST"])
-@cross_origin()
-def setmenus():
-
-    content = request.get_json()
-
-    if content is None:
-        return JSONResponse(BadRequest)
-
-    if (
-        len(content) == 1
-        and "choices" in content
-        and not request.headers.get("Authorization") is None
-    ):
-        try:
-            authorization_token = request.headers.get("Authorization").split("Bearer ")[
-                1
-            ]
-            if len(authorization_token) != 288:
-                raise ValueError("Invalid token length")
-        except:
-            return JSONResponse(BadRequest)
-
-        menus_result = api_lib.SetMenus(authorization_token, content["choices"])
-        return JSONResponse(menus_result)
-
-    else:
-        return JSONResponse(BadRequest)
-
-
-@app.route("/setcheckouts", methods=["POST"])
-@cross_origin()
-def setcheckouts():
-
-    content = request.get_json()
-
-    if content is None:
-        return JSONResponse(BadRequest)
-
-    if (
-        len(content) == 1
-        and "checkouts" in content
-        and not request.headers.get("Authorization") is None
-    ):
-        try:
-            authorization_token = request.headers.get("Authorization").split("Bearer ")[
-                1
-            ]
-            if len(authorization_token) != 288:
-                raise ValueError("Invalid token length")
-        except:
-            return JSONResponse(BadRequest)
-
-        checkouts_result = api_lib.SetCheckouts(
-            authorization_token, content["checkouts"]
-        )
-        return JSONResponse(checkouts_result)
-
-    else:
-        return JSONResponse(BadRequest)
-
-
-@app.route("/about", methods=["GET"])
-@cross_origin()
-def version():
-    return JSONResponse(About())
-
-
-@app.route("/", methods=["GET"])
-def root():
-    return redirect(About()["documentation"], code=302)
-
-
-@app.errorhandler(404)
-@cross_origin()
-def handler(e):
-    return JSONResponse({"error": True, "status_code": 404, "message": "Not found"})
-
-
-@app.route("/coffeepot", methods=["GET"])
-@cross_origin()
-def getcoffee():
-    parsed_ua = parse(request.headers.get("User-Agent"))
-    if parsed_ua.is_pc or parsed_ua.is_tablet or parsed_ua.is_mobile:
-        return HTMLGetCoffee()
-    else:
-        return JSONResponse(GetCoffee)
-
-
-@app.route("/teapot")
-@cross_origin()
-def teapot():
-    return JSONResponse(ImATeapot)
-
-
-if __name__ == "__main__":
-    app.run(port=80)
+asyncio.run(main())
